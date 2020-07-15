1e8960d0ea31258b371379e8bbdf7ac51b615ed2
codeFunny71 <whynotlaugh@gmail.com>
2020-07-15 13:09:29 -0700

    javascript practice from udemy course

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,11 @@
+let currentResult = 0;
+let currentResult = defaultResult;
+
+function add(){
+    const calcDescription = `${currentResult} + ${userInput.value}`;
+    currentResult = currentResult + parseInt(userInput.value);
+    outputResult(currentResult, calcDescription);
+}
+
+addBtn.addEventListener('click', add);
+
